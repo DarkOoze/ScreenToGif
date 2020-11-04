71248ada66e7ae36f9c45243bd465d3868d28f40
Anders Hörnfeldt <hornfeldt@gmail.com>
2020-11-04 21:36:51 +0100

    Update to sdk based project format

--- ScreenToGif/Properties/AssemblyInfo.cs
+++ ScreenToGif/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ScreenToGif")]
-[assembly: AssemblyDescription("Screen, webcam and sketchboard recorder, with integrated editor!")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nicke Manarin")]
-[assembly: AssemblyProduct("ScreenToGif")]
-[assembly: AssemblyCopyright("Copyright© Nicke Manarin 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -37,18 +25,3 @@ using System.Windows;
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
 )]
-
-//[assembly: System.Windows.Media.DisableDpiAwareness]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.27.3.0")]
-[assembly: AssemblyFileVersion("2.27.3.0")]
