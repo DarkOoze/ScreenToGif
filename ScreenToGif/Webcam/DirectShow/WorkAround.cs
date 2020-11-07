7fb62910295e8030350d5f598bbab85e6a15342b
Anders Hörnfeldt <anders.hornfeldt@eurocon.se>
2020-11-07 11:34:23 +0100

    Disable workaround

--- ScreenToGif/Webcam/DirectShow/WorkAround.cs
+++ ScreenToGif/Webcam/DirectShow/WorkAround.cs
@@ -36,21 +36,6 @@ namespace ScreenToGif.Webcam.DirectShow
         thus .NET 'Activator.CreateInstance' fails
         */
 
-        public static object CreateDsInstance(ref Guid clsid, ref Guid riid)
-        {
-            var hr = CoCreateInstance(ref clsid, IntPtr.Zero, CLSCTX.Inproc, ref riid, out var ptrIf);
-
-            if (hr != 0 || ptrIf == IntPtr.Zero)
-                Marshal.ThrowExceptionForHR(hr);
-
-            var iu = new Guid("00000000-0000-0000-C000-000000000046");
-            hr = Marshal.QueryInterface(ptrIf, ref iu, out _);
-
-            var ooo = System.Runtime.Remoting.Services.EnterpriseServicesHelper.WrapIUnknownWithComObject(ptrIf);
-            var ct = Marshal.Release(ptrIf);
-            return ooo;
-        }
-
         [DllImport("ole32.dll")]
         private static extern int CoCreateInstance(ref Guid clsid, IntPtr pUnkOuter, CLSCTX dwClsContext, ref Guid iid, out IntPtr ptrIf);
     }
